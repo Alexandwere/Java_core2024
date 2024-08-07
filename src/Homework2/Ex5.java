645e0f73b12aa4a50f8fffad11578167bbfce0c9
Alexandwere <lazukov_ao@mail.ru>
2024-08-07 17:37:02 +0300

    1

--- src/Homework2/Ex5.java
+++ src/Homework2/Ex5.java
@@ -5,9 +5,13 @@ import java.util.Arrays;
 public class Ex5 {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        for (int i = 0; i < numbers.length; i++){
-            numbers[i] = numbers.length-i;
+
+        for (int i = 0; i < numbers.length/2; i++){
+            int num = numbers[i];
+            numbers[i] = numbers [numbers.length - 1 - i];
+            numbers[numbers.length - 1 - i] = num;
         }
+
         System.out.println(Arrays.toString(numbers));
     }
 }
