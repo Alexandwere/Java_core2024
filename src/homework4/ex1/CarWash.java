ec679d129edbc4c25c82e35cabc05cd3a0a1405a
Alexandwere <lazukov_ao@mail.ru>
2024-08-21 20:34:18 +0300

    1

--- src/homework4/ex1/CarWash.java
+++ src/homework4/ex1/CarWash.java
@@ -6,6 +6,8 @@ public class CarWash {
 
     private BigDecimal price = BigDecimal.valueOf(0);
     private BigDecimal allCost = BigDecimal.valueOf(0);
+    private final BigDecimal upPrice = BigDecimal.valueOf(4000);
+    private final BigDecimal downPrice = BigDecimal.valueOf(2000);
 
     public BigDecimal wash(Car car) {
         if (car.getLength() > 6 || car.getHigh() > 2.5 || car.getWidth() > 2) {
@@ -22,9 +24,9 @@ public class CarWash {
     public BigDecimal wash(Car[] cars) {
         for (Car car : cars) {
             if (car.getLength() > 6 || car.getHigh() > 2.5 || car.getWidth() > 2) {
-                price = BigDecimal.valueOf(4000);
+                price = upPrice;
             } else {
-                price = BigDecimal.valueOf(2000);
+                price = downPrice;
             }
             car.setClean(true);
             allCost = allCost.add(price);
